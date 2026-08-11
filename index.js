259abd5468597486d1d7804cb7d7a9db0902e902
Kyse Ryadi <kyseryadi18@gmail.com>
2026-08-11 16:55:56 +0700

    Add compiled JavaScript entrypoint for Vercel

---
+++ index.js
@@ -0,0 +1,2 @@
+// Vercel entrypoint. The TypeScript sources are compiled by `npm run build`.
+export { default } from './dist/app.js';
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import cors from 'cors';
 import express from 'express';
-import helmet from 'helmet';
+import * as helmetModule from 'helmet';
 import { env } from './config/env.js';
 import { errorHandler, notFoundHandler } from './middleware/error-handler.js';
 import { requestLogger } from './middleware/request-logger.js';
@@ -9,7 +9,7 @@ import routes from './routes/index.js';
 export const app = express();
 
 app.disable('x-powered-by');
-app.use(helmet());
+app.use(helmetModule.default());
 app.use(cors({
   origin(origin, callback) {
     if (!origin || env.corsOrigins.includes(origin)) {
